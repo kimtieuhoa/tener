f25df5e644dd47809b78e7fd06f524fc1b8431ad
kimtieuhoa <104740218+kimtieuhoa@users.noreply.github.com>
2023-03-02 22:48:25 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const generateButton = document.getElementById("generate");
+const outputElement = document.getElementById("output");
+const explosionText = document.getElementById("explosion-text");
+const bingoText = document.getElementById("bingo-text");
+
+const characters = ["て", "ね", "る", "ー"];
+
+function generateRandomCharacter() {
+  const randomIndex = Math.floor(Math.random() * characters.length);
+  return characters[randomIndex];
+}
+
+function generateRandomString() {
+  let result = "";
+  for (let i = 0; i < 4; i++) {
+    result += generateRandomCharacter();
+  }
+  return result;
+}
+
+generateButton.addEventListener("click", () => {
+  const randomString = generateRandomString();
+  outputElement.textContent = randomString;
+
+  if (randomString === "てねーる") {
+    document.body.style.backgroundImage = "url('backgrounds/tener.jpg')";
+    explosionText.style.display = "block";
+    explosionText.textContent = "爆破！";
+  } else if (randomString === "てててて" || randomString === "ねねねね" || randomString === "るるるる" || randomString === "ーーーー") {
+    document.body.style.backgroundImage = "url('backgrounds/disney.jpeg')";
+    bingoText.style.display = "block";
+    bingoText.textContent = "ビンゴ！";
+  } else {
+    document.body.style.backgroundImage = "url('backgrounds/default.png')";
+    explosionText.style.display = "none";
+    bingoText.style.display = "none";
+  }
+});
+
